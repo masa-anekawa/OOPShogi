34a4e689d25982f213414083da21b9fa7e267bf2
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-07 23:17:09 +0900

    delete Color from Player

--- OOPShogi/Player.cs
+++ OOPShogi/Player.cs
@@ -1,28 +1,15 @@
 ﻿using System;
 namespace OOPShogi
 {
-    public enum Color{
-        kWhite,
-        kBlack
-    }
-
     public class Player
     {
-        private Color _color;
-        private String _name;
+        public bool IsWhite { get; private set; }
+        public String Name{ get; private set; }
 
-        public String Name{
-            get { return _name; }
-        }
-
-        public Color Color{
-            get { return _color; }
-        }
-
-        public Player(Color col, String name)
+        public Player(bool isWhite, String name)
         {
-            _color = col;
-            _name = name;
-        }
+            IsWhite = isWhite;
+            Name = name;
+		}
     }
 }
