980887ba9ff0e7e2de9615aeae6bcd0993be5b9e
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-07 17:07:51 +0900

    add Knight

---
+++ OOPShogi/Piece/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OOPShogi.Piece
+{
+    public class Knight: BPiece
+    {
+        public Knight(bool isWhite): base(EPieceSort.kKnight, true, isWhite){}
+
+		public override bool CanPromote()
+		{
+            return base.CanPromote();
+		}
+
+		public override bool HasControlTo(Coord coord)
+		{
+            if (!IsPromoted)
+                return coord == new Coord(-2, 1) || coord == new Coord(-2, -1);
+            else
+                return IsInsideGoldControl(coord);
+		}
+	}
+}
