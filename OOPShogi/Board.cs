75999846c6070bcfecc50f002b730a3123efb3e3
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-07 17:08:51 +0900

    edit Board

--- OOPShogi/Board.cs
+++ OOPShogi/Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Diagnostics;
 using System.Collections.Generic;
 
@@ -10,42 +11,62 @@ namespace OOPShogi
     {
         public readonly Coord kSize;
         private List<BPiece> _pieces;
+        private Dictionary<BPiece, Coord> _pieceCoordMap;
 
         public Board(Coord coord)
         {
             kSize = coord;
             _pieces = new List<BPiece>(coord.row * coord.col);
+            _pieceCoordMap = new Dictionary<BPiece, Coord>();
         }
 
-        public BPiece GetBPiece(Coord coord){
+        // may return null
+        public BPiece GetPiece(Coord coord){
             if (!coord.IsWithin(kSize))
             {
                 Trace.TraceError($"Board{coord} is out of the board.");
                 Environment.Exit(1);
             }
-            return _pieces[calcBPiecesIndex(coord)];
+            return _pieces[calcPiecesIndex(coord)];
         }
-        
-        public bool SetBPiece(Coord coord, BPiece piece){
-            if (GetBPiece(coord) != null)
+
+        // overwrite is not allowed
+        public bool SetPiece(Coord coord, BPiece piece){
+            if (GetPiece(coord) != null)
             {
                 Trace.TraceError($"error: Board{coord} is already occupied.");
                 return false;
             }
-            _pieces[calcBPiecesIndex(coord)] = piece;
+            _pieceCoordMap.Add(piece, coord);
+            _pieces[calcPiecesIndex(coord)] = piece;
             return true;
         }
 
+        // returned value is non-nullable
+        public Coord GetCoord(BPiece piece) => _pieceCoordMap[piece];
+
+        // overwrite is not allowed
+        public void SetCoord(BPiece piece, Coord coord){
+            _pieceCoordMap.Add(piece, coord);
+        }
+
         public bool isMovable(BPiece piece, Coord to){
+            Coord from = _pieceCoordMap[piece];
+            if (!_pieceCoordMap.ContainsKey(piece)) return false;
+            if (!to.IsWithin(kSize)) return false;
+            if (!piece.HasControlTo(to)) return false;
+            if (!piece.CanJump && !this.IsEmptyBetween(from, to)) return false;
+
+            return true;
+        }
+        public bool isMovable(Coord from, Coord to) => isMovable(GetPiece(from), to);
+
+        private bool IsEmptyBetween(Coord from, Coord to){
             // TODO
             return true;
         }
 
-        public bool isMovable(Coord from, Coord to){
-            return isMovable(GetBPiece(from), to);
-        }
-
-        private int calcBPiecesIndex(Coord coord){
+        private int calcPiecesIndex(Coord coord){
             return coord.row * kSize.col + coord.col;
         }
     }
