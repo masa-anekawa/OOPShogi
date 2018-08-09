d1c5aa7135a9759bb781b877c946425b69d1dcab
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-10 01:24:58 +0900

    add Turn

---
+++ OOPShogi/Turn.cs
@@ -0,0 +1,20 @@
+﻿namespace OOPShogi
+{
+    public class Turn
+    {
+        public bool IsWhite { get; private set; }
+
+        public Turn(bool isWhite = true){
+            IsWhite = isWhite;
+        }
+
+        public void Switch(){
+            IsWhite = !IsWhite;
+        }
+
+        public override string ToString()
+        {
+            return $"[Turn: {(IsWhite ? "White" : "Black")}]";
+        }
+    }
+}
