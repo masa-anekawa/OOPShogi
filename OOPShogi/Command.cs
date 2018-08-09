44a1663a8d90d66277e6cc5146e2a1688ede7523
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-10 01:23:22 +0900

    add command.cs

---
+++ OOPShogi/Command.cs
@@ -0,0 +1,25 @@
+﻿namespace OOPShogi
+{
+    public enum ECommandSort{
+        kMoveRequest,
+        kMoveRetraction,
+        kSurrender,
+        kDrawProposal,
+        kDrawAcceptance,
+        kDrawRejection,
+    }
+
+    public class Command
+    {
+        public ECommandSort Sort { get; private set; }
+
+        public Command(ECommandSort sort){
+            Sort = sort;
+        }
+
+        public override string ToString()
+        {
+            return $"[Command: {Sort}]";
+        }
+    }
+}
