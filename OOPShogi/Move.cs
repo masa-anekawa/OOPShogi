e8753b2bbc041bbcbc60765a3768a9c1b470e8a2
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-10 01:23:54 +0900

    add Move

---
+++ OOPShogi/Move.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using OOPShogi.Piece;
+
+namespace OOPShogi
+{
+    /// <summary>
+    /// 着手．生成時点で常に合法手であることが保証されている．
+    /// </summary>
+    public class Move
+    {
+        public BPiece piece;
+        public Coord to;
+        public bool isWhite;
+        public bool doPromote;
+        public int index;
+
+        public Move(
+            BPiece _piece,
+            Coord _to,
+            bool _isWhite,
+            bool _doPromote,
+            int _index)
+        {
+            piece = _piece;
+            to = _to;
+            isWhite = _isWhite;
+            doPromote = _doPromote;
+            index = _index;
+        }
+    }
+}
