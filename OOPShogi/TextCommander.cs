a1c9b8ca2efae43a2b927be49615fa319066dd23
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-10 01:24:44 +0900

    add TextCommander.cs

---
+++ OOPShogi/TextCommander.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+using OOPShogi.Piece;
+
+namespace OOPShogi
+{
+    public class TextCommander
+    {
+        private TextReader _textReader;
+
+        public TextCommander(TextReader reader){
+            _textReader = reader;
+        }
+        public Command AskCommand(){
+            Command command = null;
+            Console.WriteLine($"Command? ");
+            while (true)
+            {
+                string str = _textReader.ReadLine();
+                command = GenerateCommand(str);
+                if(command == null)
+                {
+                    // TODO: output design
+                    Console.WriteLine("invalid command. try again.");
+                    continue;
+                }
+                else 
+                    break;
+            }
+            return command;
+        }
+
+        private Command GenerateCommand(string str)
+        {
+            if (str == "surrender")
+                return new Command(ECommandSort.kSurrender);
+            else
+                return null;
+        }
+    }
+}
