9f11620d8ca5e6771ae17e0740de1da5c76f6e68
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-10 01:23:39 +0900

    add IPosition

---
+++ OOPShogi/IPosition.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPShogi
+{
+    /// <summary>
+    /// 局面を取得するためのinterface.
+    /// </summary>
+    public interface IPosition
+    {
+        Board GetBoard();
+        Pool GetPool(bool isWhite);
+        Turn GetTurn();
+    }
+}
