14e021634a78adb3be601e6063fd41d9b25b192e
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-11 06:15:18 +0900

    Add HistoryEvent

---
+++ OOPShogi/HistoryEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPShogi
+{
+    public struct HistoryEvent
+    {
+        public Command Command;
+        // TODO: add TimeSpan
+        public HistoryEvent(Command _command){
+            Command = _command;
+        }
+        public override string ToString()
+        {
+            return "[HistoryEvent: " + Command + "]";
+        }
+    }
+}
