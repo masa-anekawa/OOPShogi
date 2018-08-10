f1af6551593aa4af5914a9b276cf0f99ab0ba7bd
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-11 06:16:58 +0900

    add some fields to Command

--- OOPShogi/Command.cs
+++ OOPShogi/Command.cs
@@ -7,19 +7,26 @@
         kDrawProposal,
         kDrawAcceptance,
         kDrawRejection,
+        kDeclaration,
+        kPass,
     }
 
     public class Command
     {
         public ECommandSort Sort { get; private set; }
+        public bool White { get; }
+        public Move? Move { get; }
 
-        public Command(ECommandSort sort){
+        public Command(ECommandSort sort, bool _white, Move? move = null)
+        {
             Sort = sort;
+            White = _white;
+            Move = move;
         }
 
         public override string ToString()
         {
-            return $"[Command: {Sort}]";
+            return $"[Command: {Sort}{(Move.HasValue ? " with " + Move : "")}]";
         }
     }
 }
