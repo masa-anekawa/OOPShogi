a8411593ede78058cdd803b45869e6c19deefb8f
Masayuki Anekawa <anekawamasayuki@gmail.com>
2018-08-11 06:17:37 +0900

    edit TextCommander

--- OOPShogi/TextCommander.cs
+++ OOPShogi/TextCommander.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.IO;
 
+using static System.Console;
+
 using OOPShogi.Piece;
 
 namespace OOPShogi
 {
-    public class TextCommander
+    public class TextCommander: ICommander
     {
         private TextReader _textReader;
 
         public TextCommander(TextReader reader){
             _textReader = reader;
         }
-        public Command AskCommand(){
+        public Command AskCommand(bool white){
             Command command = null;
-            Console.WriteLine($"Command? ");
+            // TODO: Move発行，承認，移動までの流れ
+            Write($"Command? (pass/surrender):");
             while (true)
             {
                 string str = _textReader.ReadLine();
-                command = GenerateCommand(str);
+                command = GenerateCommand(str, white);
                 if(command == null)
                 {
                     // TODO: output design
-                    Console.WriteLine("invalid command. try again.");
+                    WriteLine("invalid command. try again.");
                     continue;
                 }
                 else 
@@ -31,10 +34,13 @@ namespace OOPShogi
             return command;
         }
 
-        private Command GenerateCommand(string str)
+        private Command GenerateCommand(string str, bool white)
         {
             if (str == "surrender")
-                return new Command(ECommandSort.kSurrender);
+                // FIXME: TextCommanderを使うのは先手か後手か？
+                return new Command(ECommandSort.kSurrender, white);
+            else if (str == "pass")
+                return new Command(ECommandSort.kPass, white);
             else
                 return null;
         }
